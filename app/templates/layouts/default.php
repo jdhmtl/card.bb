1272ad87a733ed6f43ec12bc982500b1653865b4
John Hodges <john@plankdesign.com>
2015-03-28 16:41:47 -0400

    First pass front-end integration.

--- app/templates/layouts/default.php
+++ app/templates/layouts/default.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html class="no-js" lang="en">
 	<head>
 		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<link rel="stylesheet" type="text/css" href="//cloud.typography.com/6832954/712126/css/fonts.css">
 		<link rel="stylesheet" href="/assets/css/style.css">
 		<title><?= $this->title; ?></title>
 	</head>
 	<body>
-		<header></header>
-		<div id="container">
+		<div class="container">
 			<?= $this->yieldView(); ?>
 		</div>
-		<footer></footer>
 		<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
 		<script src="/assets/js/site.js"></script>
 	</body>
--- app/templates/views/profile.php
+++ app/templates/views/profile.php
@@ -1,3 +1,61 @@
+<div class="profile-wrap">
+	<div class="profile-photo">
+		<img src="/assets/img/warren_03.jpg" alt="Warren Wilansky">
+	</div>
+	<div class="profile-info">
+		<div class="profile-text-content">
+			<h1>Warren Wilansky</h1>
+			<span class="game-attended">Games Attended</span>
+			<span class="game-attended-num"><?= $this->stats['games']; ?></span>
+		</div>
+	</div>
+</div>
+
+<div class="row-four-square-wrap">
+	<div class="square dark">
+		<div class="square-text">
+			<h2>Homeruns</h2>
+			<span class="sub">Home Runs witnessed</span>
+			<span class="regular-digits"><?= $this->stats['homeruns']; ?></span>
+		</div>
+	</div>
+
+	<div class="square medium">
+		<div class="square-text">
+			<h2>Triples</h2>
+			<span class="sub">Triples witnessed</span>
+			<span class="regular-digits"><?= $this->stats['triples']; ?></span>
+		</div>
+	</div>
+
+	<div class="square dark">
+		<div class="square-text">
+			<h2>Doubles</h2>
+			<span class="sub">Doubles witnessed</span>
+			<span class="regular-digits"><?= $this->stats['doubles']; ?></span>
+		</div>
+	</div>
+
+	<div class="square medium">
+		<div class="square-text">
+			<h2>Singles</h2>
+			<span class="sub">Singles witnessed</span>
+			<span class="regular-digits"><?= $this->stats['singles']; ?></span>
+		</div>
+	</div>
+</div>
+
+<div class="big-square-row row">
+	<div class="big-square left light">
+		<div class="big-square-content">
+			<h1 class="big-heading">Grand Slam</h1>
+			<span class="sub">Grand slam witnessed aka the Big Smalami</span>
+			<span class="big-digits">05</span>
+		</div>
+	</div>
+</div>
+
+<?php /*
 <div id="card">
 	<div class="games">
 		<h4>Games Attended</h4>
@@ -40,3 +98,4 @@
 		<span><?= $this->stats['tripleplays']; ?></span>
 	</div>
 </div>
+*/ ?>
